95048a8e8a22bad15610d8a8bfa580a5a6f37081
thousif970067-ai <thousif970067@gmail.com>
2026-08-21 16:31:23 +0530

    Add files via upload

---
+++ pythonT.py
@@ -0,0 +1,144 @@
+
+
+
+name: Thousif
+usn:KUB25EEE652
+Date:21/08/2026
+
+
+# numbers = [10, 3, 5, 6, 2, 8, 9, 24, 3, 5, 6, 7, 89]
+
+# even_numbers = []
+# for num in numbers:
+#     if num % 2 == 0:
+#         even_numbers.append(num)
+# print(even_numbers)
+
+
+#reverse the spelling
+# text = "university"
+# reverse = ""
+# for i in range(len(text)):
+#     reverse = text[i] + reverse
+# print(reverse)
+
+
+
+#avg of list
+# numbers = [10, 3, 5, 6, 7, 8, 9, 24, 3, 5, 6, 7, 89]
+# avg = sum(numbers)/len(numbers)
+# print(avg)
+
+
+# numbers = [-1, 3, 34, -8, -9, 1]
+# small = min(numbers)
+# print(small)
+
+# common element in 3 list
+# a = [1, 2, 3, 4]
+# b = [3, 4, 5, 6]
+# c = [4, 5, 6, 7]
+
+# for x in a:
+#     if x in b:
+#         if x in c:
+#             print(x)
+
+#print the num not divisible by 3
+# numbers = [3, 10, 12, 54, 75, 89, 25, 23]
+# for num in numbers:
+#     if num % 3 != 0:
+#         print(num)
+
+
+#count the num of char in university
+# a ="university"
+# b =len(a)
+# print(b)
+
+
+
+
+
+# #find secoand smallest num in list
+# numbers = [10, 3, 5, 6, 7, 8, 9, 24, 3, 5, 6, 7, 89]
+# numbers.sort()
+# for num in numbers:
+#     if num > numbers[0]:
+#         print(num)
+#         break
+
+
+# #swap 1 and last num only
+# numbers = [-1, 3, 34, -8, -9, 1]
+
+# temp = numbers[0]
+# numbers[0] = numbers[-1]
+# numbers[-1] = temp
+
+# print(numbers)
+
+
+# #find the repeating valur in both list
+# a = [1, 2, 3, 4]
+# b = [3, 4, 5, 6]
+
+# for num in a:
+#     if num in b:
+#         print(num)
+
+
+
+
+# print the num divisible by 3 and 5
+# numbers = [3, 10, 15, 54, 75, 89, 25, 23]
+
+# for num in numbers:
+#     if num % 3 == 0 and num % 5 == 0:
+#         print(num)
+        
+# #  find the smallest and largest elements
+# numbers = [10, 3, 5, 6, 7, 8, 9, 24, 3, 5, 6, 7, 89]
+
+# smallest = min(numbers)
+# largest = max(numbers)
+
+# print("Smallest:", smallest)
+# print("Largest:", largest)
+
+# # swap only the first and third elements
+# numbers = [-1, 3, 34, -8, -9, 1]
+
+# numbers[0], numbers[2] = numbers[2], numbers[0]
+
+# print(numbers)
+
+# # find the non repeating values in both the list 
+# list1 = [1, 2, 3, 4]
+# list2 = [3, 4, 5, 6]
+
+# non_repeating = []
+
+# for num in list1:
+#     if num not in list2:
+#         non_repeating.append(num)
+
+# for num in list2:
+#     if num not in list1:
+#         non_repeating.append(num)
+
+# print(non_repeating)
+
+# take the number as input and square it if it is divisible by 3
+
+# num = int(input("Enter a number: "))
+
+# if num % 3 == 0:
+#     square = num ** 2
+#     print("Square:", square)
+# else:
+#     print("Number is not divisible by 3")
+
+
+
+
